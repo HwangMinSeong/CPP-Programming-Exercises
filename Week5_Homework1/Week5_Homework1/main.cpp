9cffbe1e68f29a5f206d271532f976fa95e5198f
HwangMinSeong <103173921+HwangMinSeong@users.noreply.github.com>
2022-04-14 19:21:02 +0900

    Add files via upload

---
+++ Week5_Homework1/Week5_Homework1/main.cpp
@@ -0,0 +1,17 @@
+#include"MineSweeper.h"
+
+void main()
+
+{
+
+	int total;
+
+	printf(" <Mine Sweeper>\n");
+
+	printf(" 매설할 총 지뢰 개수 입력 : ");
+
+	scanf_s("%d", &total);
+
+	playMineSweeper(total);
+
+}
